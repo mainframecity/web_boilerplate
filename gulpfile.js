b3336748e8f4165bbf42a1ee301270b485073b07
phantummm <phantummm@gmail.com>
2015-11-07 11:25:54 -0800

    Adding coffeescript and sass tasks, removing livereload stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
+    sass = require('gulp-ruby-sass'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var jsSources = [
-  // Sources to be compiled go here.
+  'components/lib/*.js',
+  'components/scripts/*.js'
+];
+
+var sassSources = [
+  'components/sass/*.scss'
+];
+
+var coffeeSources = [
+  'components/coffee/*.coffee'
 ];
 
 gulp.task('js', function() {
@@ -17,8 +25,24 @@ gulp.task('js', function() {
       .pipe(gulp.dest('js'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(jsSources, ['js']);
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+    .pipe(sass({style: 'expanded', lineNumbers: true}))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('css'))
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true  })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['sass']);
+  gulp.watch(coffeeSources, ['coffee']);
+});
+
+gulp.task('default', ['sass', 'coffee', 'js', 'watch']);
